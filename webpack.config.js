d938f396fda0c8db3f147571d04b37716ed468b7
amit-2k1 <ksamit1110@gmail.com>
2021-11-07 20:06:19 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserlist';
+}
+
+module.exports = {
+  mode: mode,
+  target: target,
+
+  entry: path.resolve(__dirname, './src/js/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+    hot: true
+  }
+};
