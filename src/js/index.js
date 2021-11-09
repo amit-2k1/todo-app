c766ead1c3e07c9bf4c23a47097648297185f84e
amit-2k1 <ksamit1110@gmail.com>
2021-11-09 17:12:45 +0530

    add todo list form can popup using btn

--- src/js/index.js
+++ src/js/index.js
@@ -82,48 +82,69 @@ const todosContainer = document.querySelector('#todos-container');
 const todosEles = document.querySelectorAll('.todos ul');
 const todoListLinks = document.querySelectorAll('#todo-list li a');
 const backBtn = todosContainer.querySelector('button.back-btn');
+const addTodoListBtn = todoListsContainer.querySelector('.add-todo-list-btn');
+const addTodoListForm = todoListsContainer.querySelector('#add-todo-list-form');
 
-function render(event, { state, activeTodos }) {
+function render(event, eventType, { state, activeTodos }) {
   switch (state) {
     case 'showingTodoLists': {
-      const id = parseInt(event.target.id.split('-')[1]);
+      switch (eventType) {
+        case 'linkClicked': {
+          const id = parseInt(event.target.id.split('-')[1]);
+          console.log(id, event.target.id);
+          if (id !== 0 && !id) return;
 
-      if (id === NaN) return;
+          if (isActiveContainer(todoListsContainer)) {
+            todoListsContainer.classList.remove('activeContainer');
+          }
 
-      if (isActiveContainer(todoListsContainer)) {
-        todoListsContainer.classList.remove('activeContainer');
+          if (!isActiveContainer(todosContainer)) {
+            todosContainer.classList.add('activeContainer');
+            todosEles[id].classList.add('activeTodos');
+            todoApp.activeTodos = id;
+          }
+
+          return 'showingTodos';
+        }
+        case 'addListBtnClicked': {
+          if (!addTodoListForm.classList.contains('activeForm')) {
+            addTodoListForm.classList.add('activeForm');
+          }
+
+          return 'showingAddListForm';
+        }
       }
-
-      if (!isActiveContainer(todosContainer)) {
-        todosContainer.classList.add('activeContainer');
-        todosEles[id].classList.add('activeTodos');
-        todoApp.activeTodos = id;
-      }
-
-      return 'showingTodos';
     }
     case 'showingTodos': {
-      if (isActiveContainer(todosContainer)) {
-        todosContainer.classList.remove('activeContainer');
-        todosEles[activeTodos].classList.remove('activeTodos');
-        todoApp.activeTodos = -1;
-      }
+      switch (eventType) {
+        case 'backBtnClicked': {
+          if (isActiveContainer(todosContainer)) {
+            todosContainer.classList.remove('activeContainer');
+            todosEles[activeTodos].classList.remove('activeTodos');
+            todoApp.activeTodos = -1;
+          }
 
-      if (!isActiveContainer(todoListsContainer)) {
-        todoListsContainer.classList.add('activeContainer');
-      }
+          if (!isActiveContainer(todoListsContainer)) {
+            todoListsContainer.classList.add('activeContainer');
+          }
 
-      return 'showingTodoLists';
+          return 'showingTodoLists';
+        }
+      }
     }
   }
 }
 
 todoListLinks.forEach((link) => {
   link.addEventListener('click', (event) => {
-    todoApp.state = render(event, todoApp);
+    todoApp.state = render(event, 'linkClicked', todoApp);
   });
 });
 
 backBtn.addEventListener('click', (event) => {
-  todoApp.state = render(event, todoApp);
+  todoApp.state = render(event, 'backBtnClicked', todoApp);
+});
+
+addTodoListBtn.addEventListener('click', (event) => {
+  todoApp.state = render(event, 'addListBtnClicked', todoApp);
 });
