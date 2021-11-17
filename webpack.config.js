5b80f3b3c52928016ae9029313331a3ffcfb0ec0
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2021-11-17 17:44:13 +0530

    favicon.ico added

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html')
+      template: path.resolve(__dirname, './src/index.html'),
+      favicon: path.resolve(__dirname, './src/public/favicon.ico')
     }),
     new MiniCssExtractPlugin({
       filename: 'style.css'
