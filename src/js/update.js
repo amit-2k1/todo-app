0dd1c281a4ee9da2004dd2bbb70529d1c23548c3
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2021-12-01 21:16:13 +0530

    loader and popup element addede

--- src/js/update.js
+++ src/js/update.js
@@ -2,6 +2,53 @@ import { createTodoLists, createTodos } from './init';
 import uniqid from 'uniqid';
 import store from './store';
 
+const performFetch = async (method, requestType, store, tokenRequired) => {
+  const loaderContainer = document.querySelector('#loader-container');
+  const loader = loaderContainer.querySelector('#loader');
+  const popupEl = document.querySelector('p.pop-up');
+
+  let token = '';
+  if (tokenRequired) {
+    token = JSON.parse(localStorage.getItem('token'));
+  }
+
+  // display loader
+  loader.classList.add('activeLoader');
+  // display loader container
+  loaderContainer.classList.add('activeContainer');
+
+  const response = await fetch(requestType, {
+    method: method,
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      store
+    })
+  });
+
+  const { message } = await response.json();
+
+  // remove loader
+  loader.classList.remove('activeLoader');
+  // remove loader container
+  loaderContainer.classList.remove('activeContainer');
+
+  console.log(message);
+
+  // display popup
+  if (message) {
+    setTimeout(() => {
+      popupEl.textContent = message;
+      popupEl.classList.add('activePopup');
+      setTimeout(() => {
+        popupEl.classList.remove('activePopup');
+      }, 5000);
+    }, 500);
+  }
+};
+
 export async function update(
   event,
   eventType,
@@ -105,9 +152,9 @@ export async function update(
         }
         case 'deleteTodoBtnClicked': {
           const path = event.path || event.composedPath();
-          const ulIndex = path.findIndex(({ tagName }) => tagName === 'UL');
-          const ul = path[ulIndex];
-          const li = ul.querySelector('li');
+          const liIndex = path.findIndex(({ tagName }) => tagName === 'LI'); // get the li element Index
+          const li = path[liIndex];
+          const ul = li.parentElement;
           const todoId = li.id;
 
           const listIndex = newTodosLists.findIndex(
@@ -118,17 +165,9 @@ export async function update(
             listIndex
           ].todos.filter(({ id }) => id != todoId);
 
-          ul.removeChild(li);
+          await performFetch('PUT', '/deletetodo', { todoId: todoId });
 
-          const { message } = await fetch('/deletetodo', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              store: {
-                todoId: todoId
-              }
-            })
-          });
+          ul.removeChild(li);
 
           newState = 'showingTodos';
 
@@ -151,19 +190,13 @@ export async function update(
           const completed =
             !newTodosLists[listIndex].todos[todoIndex].completed;
 
+          await performFetch('PUT', '/marktodo', {
+            todoId: todoId,
+            completed: completed
+          });
+
           newTodosLists[listIndex].todos[todoIndex].completed = completed;
           p.classList.toggle('completed');
-
-          const { message } = await fetch('/marktodo', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              store: {
-                todoId: todoId,
-                completed: completed
-              }
-            })
-          });
 
           newState = 'showingTodos';
 
@@ -208,26 +241,22 @@ export async function update(
             todos: []
           };
 
+          await performFetch(
+            'PUT',
+            '/addtodolist',
+            {
+              listId: newTodosList.id,
+              listName: newTodosList.listName
+            },
+            true
+          );
+
           createTodoLists([newTodosList]);
           createTodos(newTodosList);
           newTodosLists.push(newTodosList);
 
           // closing the form after the new todo list added
           addTodoListForm.classList.remove('activeForm');
-
-          const { listId, message } = await fetch('/addtodolist', {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: JSON.parse(localStorage.getItem('token'))
-            },
-            body: JSON.stringify({
-              store: {
-                listId: newTodosList.id,
-                listName: newTodosList.listName
-              }
-            })
-          });
 
           newState = 'showingTodoLists';
 
@@ -273,26 +302,18 @@ export async function update(
 
           const id = newTodosLists.findIndex(({ id }) => id === newActiveTodos);
 
+          await performFetch('PUT', '/addtodo', {
+            todoId: newTodo.id,
+            content: newTodo.content,
+            completed: newTodo.completed,
+            listId: newTodosLists[id].id
+          });
+
           newTodosLists[id].todos.push(newTodo);
           createTodos(newTodosLists[id]);
 
           // closing the form after the new todo added
           addTodoForm.classList.remove('activeForm');
-
-          const { todoId, message } = await fetch('/addtodo', {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              store: {
-                todoId: newTodo.id,
-                content: newTodo.content,
-                completed: newTodo.completed,
-                listId: newTodosLists[id].id
-              }
-            })
-          });
 
           newState = 'showingTodos';
 
