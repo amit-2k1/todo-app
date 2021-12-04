64aa3a6cddfbcd08b466ef296df027700e9320ff
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2021-12-04 14:40:42 +0530

    .

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -6,16 +6,16 @@ if (process.env.NODE_ENV !== 'production') {
 
 const env = process.env.NODE_ENV || 'development';
 
-const db_port = process.env.DB_PORT;
-const hostname = process.env.HOSTNAME;
-const username = process.env.DB_USERNAME;
-const password = process.env.PASSWORD;
-const db_name = process.env.DB_NAME;
-
 let connectionString;
 let ssl;
 
 if (env === 'development') {
+  const db_port = process.env.DB_PORT;
+  const hostname = process.env.HOSTNAME;
+  const username = process.env.DB_USERNAME;
+  const password = process.env.PASSWORD;
+  const db_name = process.env.DB_NAME;
+
   connectionString = `postgres://${username}:${password}@${hostname}:${db_port}/${db_name}`;
   ssl = false;
 } else {
